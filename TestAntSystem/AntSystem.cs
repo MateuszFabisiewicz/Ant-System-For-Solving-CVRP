7ad5d624907c4b9d8aca9712732df9876caf2f47
MateuszFabisiewicz <01151578@pw.edu.pl>
2023-03-27 00:17:29 +0200

    added second test for AS

--- TestAntSystem/AntSystem.cs
+++ TestAntSystem/AntSystem.cs
@@ -84,6 +84,28 @@ namespace TestAntSystem
         private int numberOfAnts;
         private int seed;
         private int optimalValue;
+        public double valueWorseThan20Normal=0;
+        public double valueWorseThan10Normal=0;
+        public double valueWorseThan5Normal=0;
+        public double valueWorseThan20Elitist = 0;
+        public double valueWorseThan10Elitist = 0;
+        public double valueWorseThan5Elitist = 0;
+        public double valueWorseThan20Rank = 0;
+        public double valueWorseThan10Rank = 0;
+        public double valueWorseThan5Rank = 0;
+        public double valueNormal=0;
+        public double valueElitist=0;
+        public double valueRank=0;
+        private bool first20Normal = false;
+        private bool first10Normal = false;
+        private bool first5Normal = false;
+        private bool first20Elitist = false;
+        private bool first10Elitist = false;
+        private bool first5Elitist = false;
+        private bool first20Rank = false;
+        private bool first10Rank = false;
+        private bool first5Rank = false;
+
 
         public int sMax;
         public int[] demandsOfCities; // indeksy miast od 1, 0 to indeks magazynu
@@ -261,6 +283,61 @@ namespace TestAntSystem
                 text += $"Mrówka {id} znalazła rozwiązanie: " + ant.pathLength + " w iteracji: " + iteration + $"  Wynik gorszy od optymalnego o {Math.Round(((double)ant.pathLength / optimalValue - 1) * 100, 3)}%\n";
                 bestPath = ant.Path.ToArray();
                 bestSolutionLength = ant.pathLength;
+
+                if(Math.Round(((double)ant.pathLength / optimalValue - 1) * 100, 3) <= 20)
+                {
+                    if (TypeOfSolution.Normal == type && !first20Normal)
+                    {
+                        valueWorseThan20Normal += iteration;
+                        first20Normal = true;
+                    }
+                    if (TypeOfSolution.Elitist == type && !first20Elitist)
+                    {
+                        valueWorseThan20Elitist += iteration;
+                        first20Elitist = true;
+                    }
+                    if (TypeOfSolution.Rank == type && !first20Rank)
+                    {
+                        valueWorseThan20Rank += iteration;
+                        first20Rank = true;
+                    }
+                }
+                if (Math.Round(((double)ant.pathLength / optimalValue - 1) * 100, 3) <= 10)
+                {
+                    if (TypeOfSolution.Normal == type && !first10Normal)
+                    {
+                        valueWorseThan10Normal += iteration;
+                        first10Normal = true;
+                    }
+                    if (TypeOfSolution.Elitist == type && !first10Elitist)
+                    {
+                        valueWorseThan10Elitist += iteration;
+                        first10Elitist = true;
+                    }
+                    if (TypeOfSolution.Rank == type && !first10Rank)
+                    {
+                        valueWorseThan10Rank += iteration;
+                        first10Rank = true;
+                    }
+                }
+                if (Math.Round(((double)ant.pathLength / optimalValue - 1) * 100, 3) <= 5)
+                {
+                    if (TypeOfSolution.Normal == type && !first5Normal)
+                    {
+                        valueWorseThan5Normal += iteration;
+                        first5Normal = true;
+                    }
+                    if (TypeOfSolution.Elitist == type && !first5Elitist)
+                    {
+                        valueWorseThan5Elitist += iteration;
+                        first5Elitist = true;
+                    }
+                    if (TypeOfSolution.Rank == type && !first5Rank)
+                    {
+                        valueWorseThan5Rank += iteration;
+                        first5Rank = true;
+                    }
+                }
             }   
         }
 
@@ -330,7 +407,7 @@ namespace TestAntSystem
 
         private void PrintBestSolution(ref string text)
         {
-            Console.WriteLine($"Rozwiązanie dla ziarna{seed}: " + bestSolutionLength);
+            Console.WriteLine($"Rozwiązanie dla ziarna {seed}: " + bestSolutionLength);
             text += $"Rozwiązanie dla ziarna {seed}: " + bestSolutionLength + '\n';
 
             int routeNumber = 0;
@@ -393,7 +470,12 @@ namespace TestAntSystem
                 Ant ant = new(capacity, warehouse, demandsOfCities.Length);
                 FindSolution(ant, 0, 0, ref text, type);
             }
-
+            if (TypeOfSolution.Elitist == type)
+                valueElitist += bestSolutionLength;
+            if (TypeOfSolution.Rank == type)
+                valueRank += bestSolutionLength;
+            if (TypeOfSolution.Normal == type)
+                valueNormal += bestSolutionLength;
             PrintBestSolution(ref text);
             return text;
         }
@@ -425,11 +507,32 @@ namespace TestAntSystem
             for (int i = 0; i < numberOfEdges; i++)
                 for (int j = 0; j < numberOfEdges; j++)
                     this.pheromones[i, j] = defaultPheromoneStartingValue;
+
+            first20Normal = false;
+            first20Elitist = false;
+            first20Rank = false;
+            first10Normal = false;
+            first10Elitist = false;
+            first10Rank = false;
+            first5Normal = false;
+            first5Elitist = false;
+            first5Rank = false;
+
         }
 
         public void ChangeSeed(int newSeed)
         {
             seed = newSeed;
+        }
+
+        public void ChangeAlfa(double newAlfa)
+        {
+            pheromonePriority = newAlfa;
+        }
+
+        public void ChangeBeta(double newBeta)
+        {
+            heuristicPriority = newBeta;
         }
     }
 }
--- TestAntSystem/Program.cs
+++ TestAntSystem/Program.cs
@@ -1,23 +1,46 @@
 ﻿using System.IO;
+using System.Reflection.PortableExecutable;
 
 namespace TestAntSystem
 {
+    public struct AlfaBeta
+    {
+        public double alfa;
+        public double beta;
+
+        public AlfaBeta(double alfa, double beta)
+        {
+            this.alfa = alfa;
+            this.beta = beta;
+        }
+    }
+    
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            string[] fileNames = { "A-n37-k5.vrp","A-n45-k7.vrp","P-n70-k10.vrp","P-n51-k10.vrp","P-n23-k8.vrp", "P-n40-k5.vrp", "A-n32-k5.vrp", "E-n33-k4.vrp", "P-n16-k8.vrp"};
-
-            /*
-            string[] paths = new string[10];
-            for(int i=0;i<paths.Length;i++)
-                paths[i] = Path.Combine(Directory.GetCurrentDirectory(), @"../../../" + fileNames[i] +".txt");
-            */
-            CVRPFIlesReader[] readers = new CVRPFIlesReader[7];
+            string[] fileNames = {"P-n70-k10.vrp","P-n51-k10.vrp","P-n23-k8.vrp", "P-n40-k5.vrp","E-n33-k4.vrp", "P-n16-k8.vrp","A-n37-k5.vrp","A-n45-k7.vrp","A-n32-k5.vrp"};
+            
+            CVRPFIlesReader[] readers = new CVRPFIlesReader[8];
             for (int i = 0; i < readers.Length; i++)
                 readers[i] = new(Path.Combine(Directory.GetCurrentDirectory(), @"../../../" + fileNames[i] + ".txt"));
 
+            AlfaBeta[] alfaBetas = {
+                new(0,1),
+                new(1,0),
+                new(2,3),
+                new(3,2),
+                new(1,1),
+                new(2,2),
+                new(3,3),
+                new(3,1),
+                new(1,3),
+                new(4,4),
+            };
+
+            int[] seeds = { 1,2,3,4,5,6,7,8,9,10};
+            /*
             Parallel.ForEach(readers,
                 reader =>
                 {
@@ -50,6 +73,73 @@ namespace TestAntSystem
                     File.AppendAllText(pathElitist, textElitist);
                     File.AppendAllText(pathRank, textRank);
                     File.AppendAllText(pathGreedy, textGreedy);
+                });
+            */
+            /*
+            int[] demands = readers[6].GetDemands();
+            int numberOfAnts = 2 * demands.Length - 2;
+            AntSystem antSystem = new(numberOfAnts, demands, readers[6].GetDistances(), 3, 2, 2, 0.1, 2500, readers[6].GetCapacity(), numberOfAnts / 3, numberOfAnts / 12, 1, readers[6].GetOptimalValue());
+            antSystem.AntSystemSoultion(TypeOfSolution.Normal);
+            antSystem.AntSystemSoultion(TypeOfSolution.Elitist);
+            antSystem.AntSystemSoultion(TypeOfSolution.Rank);
+
+            antSystem = new(numberOfAnts, demands, readers[6].GetDistances(), 2, 3, 2, 0.1, 2500, readers[6].GetCapacity(), numberOfAnts / 3, numberOfAnts / 12, 1, readers[6].GetOptimalValue());
+            antSystem.AntSystemSoultion(TypeOfSolution.Normal);
+            antSystem.AntSystemSoultion(TypeOfSolution.Elitist);
+            antSystem.AntSystemSoultion(TypeOfSolution.Rank);
+            */
+            Parallel.ForEach(readers,
+                reader =>
+                {
+                    int[] demands = reader.GetDemands();
+                    int numberOfAnts = 2 * demands.Length - 2;
+                    string pathNormal = Path.Combine(Directory.GetCurrentDirectory(), @"../../../" + reader.GetName() + ".Normal6.txt");
+                    string pathElitist = Path.Combine(Directory.GetCurrentDirectory(), @"../../../" + reader.GetName() + ".Elitist6.txt");
+                    string pathRank = Path.Combine(Directory.GetCurrentDirectory(), @"../../../" + reader.GetName() + ".Rank6.txt");
+                    string textNormal = $"Liczba mrówek: {numberOfAnts}\nLiczba ciężarówek: {reader.GetNumberOfTrucks()}\n";
+                    string textElitist = $"Liczba mrówek: {numberOfAnts}\nLiczba ciężarówek: {reader.GetNumberOfTrucks()}\nLiczba elitystycznych mrówek: {numberOfAnts / 3}\n";
+                    string textRank = $"Liczba mrówek: {numberOfAnts}\nLiczba ciężarówek: {reader.GetNumberOfTrucks()}\nLiczba rangowych mrówek: {numberOfAnts / 12}\n"; ;
+                    string textGreedy = $"Liczba mrówek: {numberOfAnts}\nLiczba ciężarówek: {reader.GetNumberOfTrucks()}\n";
+                    File.AppendAllText(pathNormal, textNormal);
+                    File.AppendAllText(pathElitist, textElitist);
+                    File.AppendAllText(pathRank, textRank);
+                    Parallel.ForEach(alfaBetas,
+                        alfaBeta =>
+                        {
+                            Console.WriteLine("Alfa: "+alfaBeta.alfa);
+                            Console.WriteLine("Beta: " + alfaBeta.beta);
+                            AntSystem antSystem = new(numberOfAnts, demands, reader.GetDistances(), alfaBeta.alfa, alfaBeta.beta, 2, 0.1, 2500, reader.GetCapacity(), numberOfAnts / 3, numberOfAnts / 12, 1, reader.GetOptimalValue());
+                            Console.WriteLine("Ant System:");
+                            string textNormal = "Ant System: \n" + "Alfa: " + alfaBeta.alfa + " Beta: " + alfaBeta.beta + '\n';
+                            Console.WriteLine("Elitist Ant System:");
+                            string textElitist = "Elitist Ant System: \n" + "Alfa: " + alfaBeta.alfa + " Beta: " + alfaBeta.beta + '\n';
+                            Console.WriteLine("Rank Ant System:");
+                            string textRank = "Rank Ant System: \n" + "Alfa: " + alfaBeta.alfa + " Beta: " + alfaBeta.beta + '\n';
+                            for(int i=2;i<12;i++)//Parallel.ForEach(seeds, seed =>
+                            {
+                                antSystem.AntSystemSoultion(TypeOfSolution.Normal);
+                                antSystem.AntSystemSoultion(TypeOfSolution.Elitist);
+                                antSystem.AntSystemSoultion(TypeOfSolution.Rank);
+                                antSystem.ChangeSeed(i);
+                            };
+                            textNormal += "Wartość średnia: "+antSystem.valueNormal/10;
+                            textNormal += "\nLiczba iteracji dla której wynik jest gorszy o co najwyżej 20%: " + antSystem.valueWorseThan20Normal / 10;
+                            textNormal += "\nLiczba iteracji dla której wynik jest gorszy o co najwyżej 10%: " + antSystem.valueWorseThan10Normal / 10;
+                            textNormal += "\nLiczba iteracji dla której wynik jest gorszy o co najwyżej 5%: " + antSystem.valueWorseThan5Normal / 10;
+
+                            textElitist += "Wartość średnia: " + antSystem.valueElitist / 10;
+                            textElitist += "\nLiczba iteracji dla której wynik jest gorszy o co najwyżej 20%: " + antSystem.valueWorseThan20Elitist / 10;
+                            textElitist += "\nLiczba iteracji dla której wynik jest gorszy o co najwyżej 10%: " + antSystem.valueWorseThan10Elitist / 10;
+                            textElitist += "\nLiczba iteracji dla której wynik jest gorszy o co najwyżej 5%: " + antSystem.valueWorseThan5Elitist / 10;
+
+                            textRank += "Wartość średnia: " + antSystem.valueRank / 10;
+                            textRank += "\nLiczba iteracji dla której wynik jest gorszy o co najwyżej 20%: " + antSystem.valueWorseThan20Rank / 10;
+                            textRank += "\nLiczba iteracji dla której wynik jest gorszy o co najwyżej 10%: " + antSystem.valueWorseThan10Rank / 10;
+                            textRank += "\nLiczba iteracji dla której wynik jest gorszy o co najwyżej 5%: " + antSystem.valueWorseThan5Rank / 10;
+                            File.AppendAllText(pathNormal, textNormal);
+                            File.AppendAllText(pathElitist, textElitist);
+                            File.AppendAllText(pathRank, textRank);
+                        });
                 });
         }
 
